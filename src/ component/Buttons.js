60cb56c713ca8614e2d751662c077d9231b86c90
hanur92 <rkdgksdjfwkd92@gmail.com>
2020-11-04 18:19:40 +0900

    🔧sda

--- src/ component/Buttons.js
+++ src/ component/Buttons.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 function Buttons({ numberProp, onClickProp }) {
   return (
     <div>
-      <button onClick={onClickProp}>{numberProp}</button>
+      <button onClick={onClickProp}>{numberProp}s</button>
     </div>
   );
 }
